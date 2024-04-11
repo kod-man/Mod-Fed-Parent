77338eb38db293abd0fd6b431c5c9397976b2ed3
kodman <ufuk.kodman@gmail.com>
2024-04-11 14:25:20 +0200

    initial parent app setup

---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+function MyButton() {
+  return <div>Button</div>;
+}
+
+export default MyButton;
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import federation from "@originjs/vite-plugin-federation";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: "parent",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./MyButton": "./src/components/Button",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
